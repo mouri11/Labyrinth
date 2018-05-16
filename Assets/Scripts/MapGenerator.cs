1dc4cfccc6ae1cbec627eccbb921438c74dd9ca4
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2018-05-16 14:27:02 +0530

    Shifting from PC to Laptop
    Because apparently VS2017 has expitred -_-
    
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -21,9 +21,11 @@ public class MapGenerator : MonoBehaviour {
 	}
 
 	void Update() {
+        /*
 		if (Input.GetMouseButtonDown(0)) {
 			GenerateMap();
 		}
+        */
 	}
 
 	void GenerateMap() {
